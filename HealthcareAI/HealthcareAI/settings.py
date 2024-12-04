2a4470c7eca0e216a44dd555a90a55e616438961
Ryan Chen <ryanchen175@gmail.com>
2024-12-03 21:16:43 -0800

    Change project to use uv, add basic route

--- HealthcareAI/HealthcareAI/settings.py
+++ HealthcareAI/HealthcareAI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-avqvt2p-en5yx(%4e=_vo)o6l#aw4__ge07kk+*tzvmov^ob5m"
+SECRET_KEY = "django-insecure-q&q@cwpn6a0+m(^2n#smfu$xje3q=8ovr!chg708o$bmm0h4px"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
