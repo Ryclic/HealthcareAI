080f59f11abe0cf5fdad06c2fd1449a5c0127c40
Andrew <andrewpai736@gmail.com>
2024-12-06 08:10:59 -0800

    Add auth endpoints

--- HealthcareAI/auth/views.py
+++ HealthcareAI/auth/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def login(request):
+    return render(request, "login.html")
+
+def signuabout(request):
+    return render(request, "signup.html")
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # HeartAI
-AI health detection project for Special Topics in CS class.  
-Group Members: Ryan, Andrew, Justin, Ronnie  
+AI health detection project on heart diagnosis for Special Topics in CS class.  
+Group Members: Ryan, Andrew, Justin, Ronnie
