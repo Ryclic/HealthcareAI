d589de3c774083833fad3f8388c49714beb6133e
Ryclic <ryanchen175@gmail.com>
2025-01-08 21:26:32 -0800

    Cleanup model.py

--- HealthcareAI/prediction/model.py
+++ HealthcareAI/prediction/model.py
@@ -6,20 +6,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.metrics import precision_recall_curve
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-pd.options.display.float_format = '{:.2f}'.format
-import warnings
-warnings.filterwarnings('ignore')
 
 data = pd.read_csv('heart.csv')
 data.head()
 
-
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 df1 = data.copy(deep = True)
 df1['Sex'] = le.fit_transform(df1['Sex'])
@@ -28,48 +22,26 @@ df1['RestingECG'] = le.fit_transform(df1['RestingECG'])
 df1['ExerciseAngina'] = le.fit_transform(df1['ExerciseAngina'])
 df1['ST_Slope'] = le.fit_transform(df1['ST_Slope'])
 
-
+# from testing, these values are too statistically varied to be used - unnecessary for training
 features = df1[df1.columns.drop(['HeartDisease','RestingBP','RestingECG'])].values
 target = df1['HeartDisease'].values
 x_train, x_test, y_train, y_test = train_test_split(features, target, test_size = 0.20, random_state = 2)
 
 def model(classifier):
     classifier.fit(x_train,y_train)
-    # test stuff
-    print(len(x_train), len(y_train))
-    for value in range(0,50):
-        print("current index: " + str(x_test[value]) + ": actual " + str(y_test[value]))
-    # end test stuff
     prediction = classifier.predict(x_test)
     cv = RepeatedStratifiedKFold(n_splits = 10,n_repeats = 3,random_state = 1)
     print("Accuracy : ",'{0:.2%}'.format(accuracy_score(y_test,prediction)))
-    print("Cross Validation Score : ",'{0:.2%}'.format(cross_val_score(classifier,x_train,y_train,cv = cv,scoring = 'roc_auc').mean()))
-    print("ROC_AUC Score : ",'{0:.2%}'.format(roc_auc_score(y_test,prediction)))
-    plt.title('ROC_AUC_Plot')
-    plt.show()
 
-def model_evaluation(classifier):
-    
-    # Confusion Matrix
-    cm = confusion_matrix(y_test,classifier.predict(x_test))
-    names = ['True Neg','False Pos','False Neg','True Pos']
-    counts = [value for value in cm.flatten()]
-    percentages = ['{0:.2%}'.format(value) for value in cm.flatten()/np.sum(cm)]
-    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(names,counts,percentages)]
-    labels = np.asarray(labels).reshape(2,2)
-    sns.heatmap(cm,annot = labels,cmap = colors,fmt ='')
-    
-    # Classification Report
-    print(classification_report(y_test,classifier.predict(x_test)))
-
-
-
-
-classifier_lr = LogisticRegression(random_state = 0,C=10,penalty= 'l2') 
-print(classifier_lr)
+# more iterations to ensure convergence
+classifier_lr = LogisticRegression(solver="lbfgs", max_iter=1000)
 model(classifier_lr)
-# begin test
+
+# testing
 sample_data = np.array([[24, 1, 0, 198, 0, 140, 0, 0, 2]])
 predicted_class = classifier_lr.predict(sample_data)
-print(predicted_class)
-# end test
+
+if predicted_class == 1:
+    print("Heart failure predicted.")
+else:
+    print("Heart failure not predicted.")
